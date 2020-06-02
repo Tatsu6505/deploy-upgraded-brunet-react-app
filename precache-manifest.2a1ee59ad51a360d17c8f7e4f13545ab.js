fb9dd83bbb5dc064169bf53087345792b3c2e6c3
Tatsu6505 <coldrain6505@gmail.com>
2020-06-02 16:59:16 -0400

    Updates

---
+++ precache-manifest.2a1ee59ad51a360d17c8f7e4f13545ab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3864992087d8f14f0995ec41c443e7be",
+    "url": "/deploy-upgraded-brunet-react-app/index.html"
+  },
+  {
+    "revision": "b22a004a895d2c10ea45",
+    "url": "/deploy-upgraded-brunet-react-app/static/js/2.26141467.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/deploy-upgraded-brunet-react-app/static/js/2.26141467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b6691f99a74d23059e",
+    "url": "/deploy-upgraded-brunet-react-app/static/js/main.525b0ff7.chunk.js"
+  },
+  {
+    "revision": "f694321d7e9688343952",
+    "url": "/deploy-upgraded-brunet-react-app/static/js/runtime-main.8e9ce509.js"
+  },
+  {
+    "revision": "91655dbf34a6a2ff34f1defee6e3541e",
+    "url": "/deploy-upgraded-brunet-react-app/static/media/agedashi-tofu.91655dbf.jpeg"
+  },
+  {
+    "revision": "bf3f46ed749e6ad6c98eaf23ef4b94d0",
+    "url": "/deploy-upgraded-brunet-react-app/static/media/karaage.bf3f46ed.jpeg"
+  },
+  {
+    "revision": "2f8badf8df6a5644d65b120b2df826e1",
+    "url": "/deploy-upgraded-brunet-react-app/static/media/miso-mackerel.2f8badf8.jpeg"
+  },
+  {
+    "revision": "c2fa556d0abfc262eeb83fec8bd934ae",
+    "url": "/deploy-upgraded-brunet-react-app/static/media/ramen.c2fa556d.jpeg"
+  },
+  {
+    "revision": "f77fd229379a3a7490d600b2d989b379",
+    "url": "/deploy-upgraded-brunet-react-app/static/media/shrimp-dumplings.f77fd229.jpeg"
+  },
+  {
+    "revision": "58dcf38ef8cd06f61df99b7c7cd53a23",
+    "url": "/deploy-upgraded-brunet-react-app/static/media/tsukemono.58dcf38e.jpeg"
+  },
+  {
+    "revision": "7462383841b19fdac538a75bce324b50",
+    "url": "/deploy-upgraded-brunet-react-app/static/media/video10.74623838.mp4"
+  }
+]);
